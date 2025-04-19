c7402ddf451948cacbcceebfb7c3f4c899e7d109
souravdey425 <42832220+souravdey425@users.noreply.github.com>
2025-04-19 19:54:05 +0530

    diabetes predictor

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Diabetes_Predictor')
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
+ 
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -15,6 +16,7 @@ def predict():
     prediction = model.predict([data])[0]
     result = "Diabetic" if prediction == 1 else "Not Diabetic"
     return render_template('index.html', prediction_text=f'Result: {result}')
-
+# return_result
 if __name__ == '__main__':
     app.run(debug=True)
+
